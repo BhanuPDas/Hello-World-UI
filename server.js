2a8d48374c8e7d74214d496d24394f9765a24c67
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2024-06-22 17:39:04 +0200

    Adding changes

--- server.js
+++ server.js
@@ -8,18 +8,11 @@ const port = 3000;
 // Read the API URL from an environment variable
 app.use(cors());
 const apiUrl = process.env.API_URL || 'http://localhost:8050/api/v1/greeting';
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading index.html');
-            return;
-        }
-        // Replace placeholder with actual API URL
-        const result = data.replace('%%API_URL%%', apiUrl);
-        res.send(result);
-    });
+    res.render('index', { apiEndpoint: apiEndpoint });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
